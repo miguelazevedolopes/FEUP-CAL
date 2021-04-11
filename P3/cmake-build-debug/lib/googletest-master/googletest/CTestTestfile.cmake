cd76ecb66767cd5103edfe136aafe3ec3bab9972
Miguel Lopes <lopesazevedom@gmail.com>
2021-04-12 00:42:17 +0100

    some changes

--- P3/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/miguel/Desktop/FACULDADE/CAL/P3/lib/googletest-master/googletest
-# Build directory: /home/miguel/Desktop/FACULDADE/CAL/P3/cmake-build-debug/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- P6/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/miguel/Desktop/FACULDADE/CAL/P6/lib/googletest-master/googlemock
-# Build directory: /home/miguel/Desktop/FACULDADE/CAL/P6/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
