2d35053c7f9a44d95e40753cc56c19522d5ac22e
Miguel Lopes <lopesazevedom@gmail.com>
2021-04-18 15:36:08 +0100

    still working on P4

---
+++ Praticas/TP4/ex2.cpp
@@ -0,0 +1,92 @@
+#include "exercises.h"
+
+bool changeMakingUnlimitedDP(unsigned int C[], unsigned int n, unsigned int T, unsigned int usedCoins[]) {
+    unsigned int coinsAux[n];
+    for(int i=0;i<n;i++){
+        usedCoins[i]=0;
+        coinsAux[i]=0;
+    }
+    int count,maxcount=99999;
+    int aux;
+    bool flag=false;
+    for(int j=0;j<n;j++) {
+        aux=T;
+        for(int i=0;i<n;i++){
+            coinsAux[i]=0;
+        }
+        count=0;
+        for (int i = j; i >= 0; i--) {
+            while (aux >= C[i]) {
+                aux -= C[i];
+                coinsAux[i] += 1;
+                count++;
+            }
+        }
+        if(count<maxcount&&aux==0){
+            for(int i=0;i<n;i++){
+                usedCoins[i]=coinsAux[i];
+            }
+            maxcount=count;
+            flag=true;
+        }
+    }
+    if (flag) return true;
+	return false;
+}
+
+
+/// TESTS ///
+#include <gtest/gtest.h>
+
+TEST(TP4_Ex2, hasChangeCanonical) {
+    unsigned int C[] = {1,2,5,10};
+    unsigned int n = 4;
+    unsigned int usedCoins[4];
+
+    EXPECT_EQ(changeMakingUnlimitedDP(C,n,13,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 1);
+    EXPECT_EQ(usedCoins[1], 1);
+    EXPECT_EQ(usedCoins[2], 0);
+    EXPECT_EQ(usedCoins[3], 1);
+
+    EXPECT_EQ(changeMakingUnlimitedDP(C,n,38,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 1);
+    EXPECT_EQ(usedCoins[1], 1);
+    EXPECT_EQ(usedCoins[2], 1);
+    EXPECT_EQ(usedCoins[3], 3);
+
+    EXPECT_EQ(changeMakingUnlimitedDP(C,n,10,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 0);
+    EXPECT_EQ(usedCoins[1], 0);
+    EXPECT_EQ(usedCoins[2], 0);
+    EXPECT_EQ(usedCoins[3], 1);
+}
+
+TEST(TP4_Ex2, hasChangeNonCanonical) {
+    unsigned int C[] = {1,4,5};
+    unsigned int n = 3;
+    unsigned int usedCoins[3];
+
+    EXPECT_EQ(changeMakingUnlimitedDP(C,n,6,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 1);
+    EXPECT_EQ(usedCoins[1], 0);
+    EXPECT_EQ(usedCoins[2], 1);
+
+    EXPECT_EQ(changeMakingUnlimitedDP(C,n,8,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 0);
+    EXPECT_EQ(usedCoins[1], 2);
+    EXPECT_EQ(usedCoins[2], 0);
+
+    EXPECT_EQ(changeMakingUnlimitedDP(C,n,7,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 2);
+    EXPECT_EQ(usedCoins[1], 0);
+    EXPECT_EQ(usedCoins[2], 1);
+}
+
+TEST(TP4_Ex2, hasNoChange) {
+    unsigned int C[] = {5,10};
+    unsigned int n = 2;
+    unsigned int usedCoins[2];
+
+    EXPECT_EQ(changeMakingUnlimitedDP(C,n,1,usedCoins), false);
+}
---
+++ Praticas/TP4/ex3.cpp
@@ -0,0 +1,98 @@
+#include "exercises.h"
+bool changeMakingDP(unsigned int C[], unsigned int Stock[], unsigned int n, unsigned int T, unsigned int usedCoins[]){
+    unsigned int coinsAux[n];
+    int count=0;
+    if(T==0){
+        return true;
+
+    }
+    for(int i=0;i<n;i++){
+        coinsAux[i]=0;
+    }
+
+}
+
+bool changeMakingDP(unsigned int C[], unsigned int Stock[], unsigned int n, unsigned int T, unsigned int usedCoins[]) {
+    int count;
+    unsigned int coinsAux[n];
+    for(int i=0;i<n;i++){
+        usedCoins[i]=0;
+        coinsAux[i]=0;
+    }
+    for (int i = n-1; i >= 0; i--) {
+        while (T >= C[i]) {
+            T -= C[i];
+            usedCoins[i] += 1;
+            count++;
+        }
+    }
+
+}
+
+/// TESTS ///
+#include <gtest/gtest.h>
+
+TEST(TP4_Ex3, hasChangeCanonical) {
+    unsigned int C[] = {1,2,5,10};
+    unsigned int Stock[] = {1,1,1,1};
+    unsigned int n = 4;
+    unsigned int usedCoins[4];
+
+    EXPECT_EQ(changeMakingDP(C,Stock,n,13,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 1);
+    EXPECT_EQ(usedCoins[1], 1);
+    EXPECT_EQ(usedCoins[2], 0);
+    EXPECT_EQ(usedCoins[3], 1);
+
+    unsigned int Stock2[] = {1,2,4,2};
+    EXPECT_EQ(changeMakingDP(C,Stock2,n,38,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 1);
+    EXPECT_EQ(usedCoins[1], 1);
+    EXPECT_EQ(usedCoins[2], 3);
+    EXPECT_EQ(usedCoins[3], 2);
+
+    unsigned int Stock3[] = {10,2,4,2};
+    EXPECT_EQ(changeMakingDP(C,Stock3,n,10,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 0);
+    EXPECT_EQ(usedCoins[1], 0);
+    EXPECT_EQ(usedCoins[2], 0);
+    EXPECT_EQ(usedCoins[3], 1);
+}
+
+TEST(TP4_Ex3, hasChangeNonCanonical) {
+    unsigned int C[] = {1,4,5};
+    unsigned int Stock[] = {2,2,1};
+    unsigned int n = 3;
+    unsigned int usedCoins[3];
+
+    EXPECT_EQ(changeMakingDP(C,Stock,n,6,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 1);
+    EXPECT_EQ(usedCoins[1], 0);
+    EXPECT_EQ(usedCoins[2], 1);
+
+    EXPECT_EQ(changeMakingDP(C,Stock,n,8,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 0);
+    EXPECT_EQ(usedCoins[1], 2);
+    EXPECT_EQ(usedCoins[2], 0);
+
+    unsigned int Stock2[] = {8,2,1};
+    EXPECT_EQ(changeMakingDP(C,Stock2,n,7,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 2);
+    EXPECT_EQ(usedCoins[1], 0);
+    EXPECT_EQ(usedCoins[2], 1);
+
+    EXPECT_EQ(changeMakingDP(C,Stock2,n,8,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 0);
+    EXPECT_EQ(usedCoins[1], 2);
+    EXPECT_EQ(usedCoins[2], 0);
+}
+
+TEST(TP4_Ex3, hasNoChange) {
+    unsigned int C[] = {1,2,5,10};
+    unsigned int Stock[] = {0,1,1,1};
+    unsigned int n = 4;
+    unsigned int usedCoins[4];
+
+    EXPECT_EQ(changeMakingDP(C,Stock,n,18,usedCoins), false);
+    EXPECT_EQ(changeMakingDP(C,Stock,n,1,usedCoins), false);
+}
